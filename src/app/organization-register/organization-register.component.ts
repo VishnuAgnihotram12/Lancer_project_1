c537a47ce62ad678942de7c20efb21d3b87eb46c
VishnuAgnihotram12 <agnihotramvishnuvardhan@gmail.com>
2024-11-24 01:20:44 +0530

    xcel-download

--- src/app/organization-register/organization-register.component.ts
+++ src/app/organization-register/organization-register.component.ts
@@ -14,18 +14,39 @@ export class OrganizationRegisterComponent {
 
   }
   ngOnInit(){
-    this.attendanceData = localStorage.getItem("AttendanceData");
+    const storedArray = localStorage.getItem("AttendanceData");
+    const parsedArray = storedArray ? JSON.parse(storedArray) : [];
+    this.attendanceData = parsedArray;
     this.organisationName = localStorage.getItem("org-name");
   }
   public take_attendance(){
    this.apiService.attendanceAPI().subscribe((result:any)=>{
     if(result && result.status === 200){
       this.attendanceData = result.data.attendance;
-      localStorage.setItem("AttendanceData",result.data.attendance);
+      localStorage.setItem("AttendanceData",JSON.stringify(result.data.attendance));
     }
    });
   }
   public add_User(){
     this.router.navigate(["Add-user"])
+  }
+  private convertToCSV(data: any[]): string {
+    if (!data.length) return '';
+    const headers = Object.keys(data[0])
+    .map(header => header.charAt(0).toUpperCase() + header.slice(1))
+    .join(',');
+    const rows = data.map(row => Object.values(row).join(',')); 
+    return [headers, ...rows].join('\n'); 
+  }
+
+  public downloadExcel(){
+    const csvData = this.convertToCSV(this.attendanceData); 
+    const blob = new Blob([csvData], { type: 'text/csv' }); 
+    const url = window.URL.createObjectURL(blob); 
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'attendanceData.csv';
+    link.click();
+    window.URL.revokeObjectURL(url);
   }
 }
