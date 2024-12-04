31da4ca70fde037fdeeaea8f040a2b76015c0ebf
VishnuAgnihotram12 <agnihotramvishnuvardhan@gmail.com>
2024-12-04 15:29:07 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,15 @@ export class AppComponent {
   navigateToSignup() {
     this.router.navigate(['/signup']);
 }
+  public userList(){
+    const user_id  = localStorage.getItem("UserId")
+    this.apiService.oragnisationList(user_id).subscribe({
+      next:(result: any)=> {
+      if(result){
+      
+      }
+    },error:(err: any)=>{
+      this.toastr.error('An error occurred. Please try again.','error',{positionClass:'toast-top-center'});
+    }});
+  }
 }
