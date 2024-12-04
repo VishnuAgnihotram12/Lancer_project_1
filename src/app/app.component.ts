4c537b187c72d63437c8e6620893bb37c71c0781
VishnuAgnihotram12 <agnihotramvishnuvardhan@gmail.com>
2024-12-04 16:06:08 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,7 +24,7 @@ export class AppComponent {
     this.apiService.oragnisationList(user_id).subscribe({
       next:(result: any)=> {
       if(result){
-      
+      localStorage.setItem("user-name",result.data.user_data.name);
       }
     },error:(err: any)=>{
       this.toastr.error('An error occurred. Please try again.','error',{positionClass:'toast-top-center'});
